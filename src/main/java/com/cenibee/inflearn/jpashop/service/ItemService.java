2462c95d1cb2b5ee00e9c6a1b9e340b42e0a2173
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-07-07 10:50:18 +0900

    :pencil2: 상품 도메인 개발

---
+++ src/main/java/com/cenibee/inflearn/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.cenibee.inflearn.jpashop.service;
+
+import com.cenibee.inflearn.jpashop.domain.item.Item;
+import com.cenibee.inflearn.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
